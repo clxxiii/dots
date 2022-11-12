815848fda10e85ec4fbb7273294b1cd2c360e1cc
clxxiii <clxxiii@users.noreply.github.com>
2022-11-12 18:04:20 -0500

    check for .config dir

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # The progam assumes the user put this directory in their home folder
 
+# Check for config folder
+[ ! -d "~/.config" ] && mkdir ~/.config
+
 # Add symlinks for each folder
 ln -s ~/.config/nvim ~/.config/nvim
 ln -s ~/.gitconfig ~/.gitconfig
