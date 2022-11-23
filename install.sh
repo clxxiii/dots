2e36b851d0b8352c3f6fc5ea8ba4284050d8a098
clxxiii <clxxiii@users.noreply.github.com>
2022-11-23 12:41:52 -0500

    add ranger config

--- install.sh
+++ install.sh
@@ -3,16 +3,26 @@
 # The progam assumes the user put this directory in their home folder
 
 # Check for config folder
-[ ! -d "~/.config" ] && mkdir ~/.config
-[ ! -d "~/Pictures" ] && mkdir ~/Pictures
+[  -d "~/.config" ] && mkdir ~/.config 
+[  -d "~/Pictures" ] && mkdir ~/Pictures 
 
 # Add symlinks for each folder
 # The file checks are to prevent a weird error in the way the symlink command works
-[ ! -d "~/.config/nvim" ] && ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
-[ ! "~/.vimrc" ] && ln -s ~/.dotfiles/.vimrc ~/.vimrc
-[ ! "~/.gitconfig" ] && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-[ ! "~/.alacritty.yml" ] && ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
-[ ! -d "~/.config/cava" ] && ln -s ~/.dotfiles/.config/cava ~/.config/cava
-[ ! -d "~/.config/bottom" ] && ln -s ~/.dotfiles/.config/bottom ~/.config/bottom
-[ ! "~/.p10k.zsh" ] && ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
-[ ! -d "~/Pictures/Wallpapers" ] && ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/bottom ~/.config/bottom
+ln -s ~/.dotfiles/.config/cava ~/.config/cava
+ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
+ln -s ~/.dotfiles/.config/ranger ~/.config/ranger
+
+ln -s ~/.dotfiles/.vimrc ~/.vimrc 
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig 
+ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# Remove double symlinks
+rm ~/.config/nvim/nvim
+rm ~/.config/cava/cava
+rm ~/.config/bottom/bottom
+rm ~/Pictures/Wallpapers/Wallpapers
+rm ~/.config/ranger/ranger
+
