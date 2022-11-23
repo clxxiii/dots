1ff741f69ab4388dc4984e88763a5b090f9965e1
clxxiii <clxxiii@users.noreply.github.com>
2022-11-23 02:32:14 -0500

    Add wallpapers & p10k config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 
 # Check for config folder
 [ ! -d "~/.config" ] && mkdir ~/.config
+[ ! -d "~/Pictures" ] && mkdir ~/Pictures
 
 # Add symlinks for each folder
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
@@ -12,3 +13,5 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s ~/.dotfiles/.config/cava ~/.config/cava
 ln -s ~/.dotfiles/.config/bottom ~/.config/bottom
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
