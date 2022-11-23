30a554166c4d13bf70ad2c73ed172b87dc5d861a
clxxiii <clxxiii@users.noreply.github.com>
2022-11-23 02:37:22 -0500

    add file checks to symlink commands

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@
 [ ! -d "~/Pictures" ] && mkdir ~/Pictures
 
 # Add symlinks for each folder
-ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
-ln -s ~/.dotfiles/.config/cava ~/.config/cava
-ln -s ~/.dotfiles/.config/bottom ~/.config/bottom
-ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
-ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
+# The file checks are to prevent a weird error in the way the symlink command works
+[ ! -d "~/.config/nvim" ] && ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+[ ! "~/.vimrc" ] && ln -s ~/.dotfiles/.vimrc ~/.vimrc
+[ ! "~/.gitconfig" ] && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+[ ! "~/.alacritty.yml" ] && ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
+[ ! -d "~/.config/cava" ] && ln -s ~/.dotfiles/.config/cava ~/.config/cava
+[ ! -d "~/.config/bottom" ] && ln -s ~/.dotfiles/.config/bottom ~/.config/bottom
+[ ! "~/.p10k.zsh" ] && ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+[ ! -d "~/Pictures/Wallpapers" ] && ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
