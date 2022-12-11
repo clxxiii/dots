c7cb10e7f46ce786ec2207f45ab5555f71e86324
clxxiii <clxxiii@users.noreply.github.com>
2022-12-11 15:02:44 -0500

    move path variables to .zshenv

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ ln -s ~/.dotfiles/.config/rofi ~/.config/rofi
 ln -s ~/.dotfiles/.config/bashtop ~/.config/bashtop
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.vimrc ~/.vimrc 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig 
 ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
