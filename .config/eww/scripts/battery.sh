2ac06c0b8e1bc2bab4418e7c498c802b47551d74
clxxiii <clxxiii@users.noreply.github.com>
2023-04-11 19:04:17 -0400

    use material icons

--- .config/eww/scripts/battery.sh
+++ .config/eww/scripts/battery.sh
@@ -7,30 +7,13 @@ icon() {
 
 [ $(cat "$bat/status") = Charging ] && echo "" && exit
 
-if [ "$per" -gt "90" ]; then
+if [ "$per" -gt "15" ]; then
 	icon=""
-elif [ "$per" -gt "80" ]; then
-	icon=""
-elif [ "$per" -gt "70" ]; then
-	icon=""
-elif [ "$per" -gt "60" ]; then
-	icon=""
-elif [ "$per" -gt "50" ]; then
-	icon=""
-elif [ "$per" -gt "40" ]; then
-	icon=""
-elif [ "$per" -gt "30" ]; then
-	icon=""
-elif [ "$per" -gt "20" ]; then
-	icon=""
 elif [ "$per" -gt "10" ]; then
-	icon=""
-	notify-send -u critical "Battery Low" "Connect Charger"
-elif [ "$per" -gt "0" ]; then
-	icon=""
+	icon=""
 	notify-send -u critical "Battery Low" "Connect Charger"
 else
-        echo  && exit
+        echo  && exit
 fi
 echo "$icon"
 }
